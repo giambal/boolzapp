b177b8f58a88cb46fda5f21d086fdfad21973197
giambal <gimmy_1996@hotmail.it>
2019-03-11 20:44:32 +0100

    jkji

--- whatsapp.js
+++ whatsapp.js
@@ -1,6 +1,27 @@
+function addMsg() {
+
+  var today= new Date();
+
+  var msgRow=$(".msg-row")
+  var msg=$(".msg");
+  var msgText=document.createElement("p");
+  var msgHourCont=document.createElement("div");
+  var check=$(".msg .fas.fa-check");
+  var ora=document.createElement("p");
+
+  msgText= document.getElementById('msg-input-box').value;
+  ora=today.getHours()+ ":"+ today.getMinutes();
+
+  msgHourCont.append(check);
+  msgHourCont.append(ora);
+  msg.append(msgText);
+  msg.append(msgHourCont);
+  msgRow.append(msg);
+}
+
 function init() {
 
-  
+addMsg();
 }
 
 $(document).ready(init);
